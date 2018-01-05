7f08352f78afc8f151b507019b33a2566778a2aa
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-01-05 16:20:00 -0500

    ive decided to let the seeding happen as users use the site.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=2',
+    response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-4519-bcf9-6d11a24ad006/setlists.json?p=1',
     :headers => { "accept" => "application/json", "x-api-key" => "#{ENV["SETLIST_API_KEY"]}"})
     @dead = JSON.parse(response.body)
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,9 @@ response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-451
 @dead = JSON.parse(response.body)
 
 
-# Tour.create(name: @dead["setlists"]['setlist'].last["@tour"])
-# Show.create(venue: @dead["setlists"]['setlist'].last["venue"]['@name'], date: @dead["setlists"]['setlist'].last['@eventDate'], tour: Tour.first )
-# Setlist.create(name: @dead["setlists"]['setlist'].last['sets']['set'][0]['@name'], show: Show.first)
-
+Tour.create(name: @dead["setlists"]['setlist'].last["@tour"])
+Show.create(venue: @dead["setlists"]['setlist'].last["venue"]['@name'], date: @dead["setlists"]['setlist'].last['@eventDate'], tour: Tour.first )
+Setlist.create(name: @dead["setlists"]['setlist'].last['sets']['set'][0]['@name'], show: Show.first)
 @dead["setlists"]['setlist'].last['sets']['set'][1]['song'].each do |song|
   Song.create(name: song['@name'], setlist: Setlist.first, length: 25)
 end
-
-  # <li> Date <%= @dead["setlists"]['setlist'].last['@eventDate'] %> </li>
-  # <li> Venue <%= @dead["setlists"]['setlist'].last["venue"]['@name']%> </li>
-  # <li><%= @dead["setlists"]['setlist'].last['sets']['set'][0]['@name'] %></li>
-
-  # <% @dead["setlists"]['setlist'].last['sets']['set'][0]['song'].each do |song| %>
-  #   <li> <%= song['@name'] %></li>
-  # <% end %>
-  # <li><%= @dead["setlists"]['setlist'].last['sets']['set'][1]['@name'] %></li>
-  # <% @dead["setlists"]['setlist'].last['sets']['set'][1]['song'].each do |song| %>
-  #   <li> <%= song['@name'] %></li>
-  # <% end %>
-  # <li>encore</li>
-  # <%= @dead["setlists"]['setlist'].last['sets']['set'][2]['song']['@name'] %>
