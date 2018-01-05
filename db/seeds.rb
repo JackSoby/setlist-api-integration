a21e6ef62727eaeae6871223271fc594eaf1f37f
Jack Sobocinski <jacksobocinski@Jacks-MacBook-Air.local>
2018-01-04 20:37:46 -0500

    add sets

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,14 @@ response = HTTParty.get('http://api.setlist.fm/rest/0.1/artist/94f8947c-2d9c-451
 # Show.create(venue: @dead["setlists"]['setlist'].last["venue"]['@name'], date: @dead["setlists"]['setlist'].last['@eventDate'], tour: Tour.first )
 # Setlist.create(name: @dead["setlists"]['setlist'].last['sets']['set'][0]['@name'], show: Show.first)
 
-
+@dead["setlists"]['setlist'].last['sets']['set'][1]['song'].each do |song|
+  Song.create(name: song['@name'], setlist: Setlist.first, length: 25)
+end
 
   # <li> Date <%= @dead["setlists"]['setlist'].last['@eventDate'] %> </li>
   # <li> Venue <%= @dead["setlists"]['setlist'].last["venue"]['@name']%> </li>
   # <li><%= @dead["setlists"]['setlist'].last['sets']['set'][0]['@name'] %></li>
+
   # <% @dead["setlists"]['setlist'].last['sets']['set'][0]['song'].each do |song| %>
   #   <li> <%= song['@name'] %></li>
   # <% end %>
